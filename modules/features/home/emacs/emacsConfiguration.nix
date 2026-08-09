c9c9ac3d0b19a2d605109dd53c79f6acfd2017ba
Stylinx <Artemis25265@gmail.com>
2026-08-09 17:59:58 -0400

    fixed emacs

--- modules/features/home/emacs/emacsConfiguration.nix
+++ modules/features/home/emacs/emacsConfiguration.nix
@@ -8,7 +8,20 @@
       poppler
       texliveFull
     ];
-    xdg.configFile."emacs".source = config.lib.file.mkOutOfStoreSymlink
-      "${./configuration}";
+
+    xdg.configFile."emacs/early-init.el".source = ./configuration/early-init.el;
+    xdg.configFile."emacs/init.el".source = ./configuration/init.el;
+    xdg.configFile."emacs/org" = {
+      source = ./configuration/org;
+      recursive = true;
+    };
+    #    xdg.configFile."emacs/org/config" = {
+    #      source = ./configuration/org/config;
+    #      recursive = true;
+    #    };
+    #    xdg.configFile."emacs/org/packages" = {
+    #      source = ./configuration/org/packages;
+    #      recursive = true;
+    #    };
   };
 }
