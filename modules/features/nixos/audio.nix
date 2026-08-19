657c335f1f8b4d1868ac9e29a8168da4894ee888
Stylinx <Artemis25265@gmail.com>
2026-08-19 19:17:09 -0400

    audio

--- modules/features/nixos/audio.nix
+++ modules/features/nixos/audio.nix
@@ -1,8 +1,13 @@
 { self, inputs, ... }: {
   flake.nixosModules.audio = { ... }: {
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
+      jack.enable = true;
     };
   };
 }
