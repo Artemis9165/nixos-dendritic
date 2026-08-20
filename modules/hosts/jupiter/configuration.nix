97cb1c75da3ed07e962a5dbd50cdb9bd7867b6c1
Stylinx <Artemis25265@gmail.com>
2026-08-20 18:32:09 -0400

    t

--- modules/hosts/jupiter/configuration.nix
+++ modules/hosts/jupiter/configuration.nix
@@ -40,15 +40,15 @@
         modesetting.enable = true;
         powerManagement = {
           enable = true;
-          finegrained = true;
+          finegrained = false;
         };
         prime = {
           offload = {
             enable = true;
             enableOffloadCmd = true;
           };
-          amdgpuBusId = "PCI:5@0:0:0";
-          nvidiaBusId = "PCI:1@0:0:0";
+          amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:0";
         };
       };
     };
