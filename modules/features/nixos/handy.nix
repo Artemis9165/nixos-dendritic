9add380e4bef4a7ab86b1ec29c00684aa39a3e84
Stylinx <Artemis25265@gmail.com>
2026-08-24 15:19:17 -0400

    test

--- modules/features/nixos/handy.nix
+++ modules/features/nixos/handy.nix
@@ -3,6 +3,7 @@
     programs.ydotool.enable = true;
     systemd.services.ydotoold = {
       serviceConfig = {
+        RuntimeDirectoryGroup = lib.mkforce "ydotool";
         RuntimeDirectoryMode = lib.mkForce "0750";
         ExecStart = lib.mkForce
           "${pkgs.ydotool}/bin/ydotoold --socket-path=/run/ydotoold/socket --socket-perm=0660";
