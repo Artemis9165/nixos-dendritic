1b88ac7c4caca4fa1e07166cb3742e477d0c1df2
Stylinx <Artemis25265@gmail.com>
2026-08-24 13:32:12 -0400

    xwayland-satellite

--- modules/features/nixos/niri.nix
+++ modules/features/nixos/niri.nix
@@ -1,6 +1,9 @@
 { self, inputs, ... }: {
   flake.nixosModules.niri = { pkgs, ... }: {
     imports = [ self.nixosModules.dms ];
+    environment.systemPackages = with pkgs; [
+      xwayland-satellite
+    ];
     programs.niri = {
       enable = true;
     };
