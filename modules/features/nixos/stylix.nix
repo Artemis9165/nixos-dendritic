dbc62d7ecd2c761a82fd914bb00a36d905835d39
Stylinx <Artemis25265@gmail.com>
2026-09-10 17:12:10 -0400

    stylix fonts

--- modules/features/nixos/stylix.nix
+++ modules/features/nixos/stylix.nix
@@ -1,10 +1,25 @@
 { self, inputs, ... }: {
-  flake.nixosModules.stylix = { pkgs, ... }: {
+  flake.nixosModules.stylix = { config, pkgs, ... }: {
     imports = [ inputs.stylix.nixosModules.stylix ];
     stylix = {
       enable = true;
       polarity = "dark";
       base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+      fonts = {
+        serif = config.stylix.fonts.sansSerif;
+        sansSerif = {
+          package = pkgs.roboto;
+          name = "Roboto";
+        };
+        monospace = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrainsMono Nerd Font Mono";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
     };
   };
 }
