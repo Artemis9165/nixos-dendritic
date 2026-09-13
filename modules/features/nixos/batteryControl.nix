36ae63a236bb29f9d3f55aae859036857198d4bd
Stylinx <Artemis25265@gmail.com>
2026-09-13 01:59:25 -0400

    tlp rework

--- modules/features/nixos/batteryControl.nix
+++ modules/features/nixos/batteryControl.nix
@@ -6,17 +6,23 @@
       tlp = {
         enable = true;
         settings = {
-          CPU_SCALING_GOVERNOR_ON_AC = "performance";
-          CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+          CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
-          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+
+          CPU_BOOST_ON_AC = 1;
+          CPU_BOOST_ON_BAT = 1;
 
           RUNTIME_PM_ON_AC = "on";
           RUNTIME_PM_ON_BAT = "on";
 
           PCIE_ASPM_ON_AC = "default";
           PCIE_ASPM_ON_BAT = "default";
+
+          PLATFORM_PROFILE_ON_AC = "balanced";
+          PLATFORM_PROFILE_ON_BAT = "balanced";
 
           STOP_CHARGE_THRESH_BAT0 = 75;
           STOP_CHARGE_THRESH_BAT1 = 75;
