cd988c4a2f5a4f08d87f8483099addfe51170b22
Stylinx <Artemis25265@gmail.com>
2026-09-19 15:26:34 -0400

    remove limine from common imports

--- modules/features/nixos/commonImports.nix
+++ modules/features/nixos/commonImports.nix
@@ -1,7 +1,6 @@
 { self, inputs, ... }: {
   flake.nixosModules.commonImports = { ... }: {
     imports = [
-      self.nixosModules.limine
       self.nixosModules.homeManager
       self.nixosModules.niri
       self.nixosModules.garbageCollection
